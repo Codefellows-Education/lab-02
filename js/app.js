7f719c18fdf2391824d67234221b18955242ce72
LENA EIVY <applena@gmail.com>
2018-10-17 09:23:23 -0700

    feature one writen out

---
+++ js/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//display all of the iamges
+  //jQuery on load
+  //$.get() to read JSON
+
+  //need a constructor function
+    //each object should become a new instance
+      //get necissary properties: url, name, description, keywords, horns
+
+  //use jQuery to make a copy of the template that we need fron HTML
+    //copy template
+    //fill in template with properties
+    //append to DOM with properties
+
+  
