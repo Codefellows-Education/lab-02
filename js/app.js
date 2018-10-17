29d5fd2a949b494b62d52a9f01cd592ad962e89b
LENA EIVY <applena@gmail.com>
2018-10-17 09:54:09 -0700

    objects exisst

--- js/app.js
+++ js/app.js
@@ -4,10 +4,28 @@
   //jQuery on load
   //$.get() to read JSON
 
-  //need a constructor function
-    //each object should become a new instance
-      //get necissary properties: url, name, description, keywords, horns
 
+
+function HornedAnimal(hornedAnimal) {
+  this.url = hornedAnimal.image_url;
+  this.name = hornedAnimal.title;
+  this.description = hornedAnimal.description;
+  this.keyword = hornedAnimal.keyword;
+  this.horns = hornedAnimal.horns;
+
+  allHornedAnimals.push(this);
+}
+
+const allHornedAnimals = [];
+
+let createHornedAnimals = () => {
+  $.getJSON('./data/page-1.json', data => {
+    data.forEach (animalObj => new HornedAnimal(animalObj));
+  });
+  console.log(allHornedAnimals);
+}
+
+createHornedAnimals();
   //use jQuery to make a copy of the template that we need fron HTML
     //copy template
     //fill in template with properties
